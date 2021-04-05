cba89d1917e515957079038eb6f8a5ff37280d61
debian-openbox <77241939+debian-openbox@users.noreply.github.com>
2021-04-05 14:48:07 +0200

    Create install_icewm-2.3.1.sh

---
+++ install_icewm-2.3.1.sh
@@ -0,0 +1,6 @@
+wget https://github.com/ice-wm/icewm/releases/download/2.3.1/icewm-2.3.1.tar.lz
+tar -x --lzip -vpf icewm-2.3.1.tar.lz
+cd icewm-2.3.1
+./configure --prefix=/usr
+make
+sudo make install
